af8ae560f65608369561a165e3972c6698716be9
MaxJanssen2002 <M.Janssen40@student.han.nl>
2024-04-02 16:00:42 +0200

    Changed system that loads models into rviz

--- implementation/launch/display.launch.py
+++ implementation/launch/display.launch.py
@@ -1,9 +1,8 @@
 import os
 import launch_ros
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
@@ -20,30 +19,16 @@ def generate_launch_description():
     robot_arm_urdf = os.path.join(get_package_share_directory('implementation'), 'urdf/lynxmotion_arm.urdf')
     cup_urdf = os.path.join(get_package_share_directory('implementation'), 'urdf/cup.urdf')
 
-    with open(robot_arm_urdf, 'r') as infp:
-        robot_arm_description = infp.read()
-
-    with open(cup_urdf, 'r') as infp:
-        cup_description = infp.read()
-
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
         DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            name='robot_state_publisher1',
+            name='robot_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time, 'robot_arm': robot_arm_description}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=[robot_arm_urdf]
-            ),
-         Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher2',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time, 'cup': cup_description}],
-            arguments=[cup_urdf]
             ),
         Node(
             package='implementation',
@@ -51,7 +36,6 @@ def generate_launch_description():
             name='state_publisher',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}],
-            
             ),
         Node(
             package='implementation',
